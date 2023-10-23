b6527ab44415a952343051aa3fe3beb023f90464
chediamdouni <62970323+chediamdouni@users.noreply.github.com>
2023-10-23 04:26:59 +0200

    fixing issue

--- resources/views/layouts/includes/leftbar.blade.php
+++ resources/views/layouts/includes/leftbar.blade.php
@@ -9,11 +9,11 @@
     <a href="{{ route('videos') }}" class="list-group-item list-group-item-action px-3 border-0">
         <i class="fas fa-video fa-lg text-danger me-3"></i> Videos</a>
 
-    <a href="{{ route('teacher_student_info') }}" class="list-group-item list-group-item-action px-3 border-0">
+    {{-- <a href="{{ route('teacher_student_info') }}" class="list-group-item list-group-item-action px-3 border-0">
         <i class="fas fa-chalkboard-teacher fa-lg text-success me-3"></i>Teachers information</a>
 
     <a href="{{ route('teacher_student_info') }}" class="list-group-item list-group-item-action px-3 border-0">
-        <i class="fas fa-user-friends fa-lg text-warning me-3"></i> Student information</a>
+        <i class="fas fa-user-friends fa-lg text-warning me-3"></i> Student information</a> --}}
 </div>
 
 <div class="card text-start">
--- resources/views/layouts/includes/navbar.blade.php
+++ resources/views/layouts/includes/navbar.blade.php
@@ -42,18 +42,18 @@ $menu = Auth::user()->id
                 </a>
             </li>
 
-            <li class="nav-item me-4" data-mdb-toggle="tooltip" data-mdb-placement="bottom" title="Class routine">
+            <li class="nav-item me-4" data-mdb-toggle="tooltip" data-mdb-placement="bottom" title="Market Place">
                 <a class="nav-link  @if ($menu == 'Product') active @endif" href="{{ route('front_product') }}">
                     <span><i class="nav-icon fas fa-calendar-alt fa-lg"></i></span>
                 </a>
             </li>
 
-            <li class="nav-item me-4" data-mdb-toggle="tooltip" data-mdb-placement="bottom" title="Teachers & Students">
+            {{-- <li class="nav-item me-4" data-mdb-toggle="tooltip" data-mdb-placement="bottom" title="Teachers & Students">
                 <a class="nav-link  @if ($menu == 't_s_info') active @endif"
                     href="{{ route('teacher_student_info') }}">
                     <span><i class="fas fa-users fa-lg"></i></span>
                 </a>
-            </li>
+            </li> --}}
         </ul>
         <!-- Center elements -->
 
